af4ad4009ee50a0af5be1a0d0c14f712be3c3dab
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-23 00:26:28 -0700

    Added seeded faker gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,12 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Tea.variety), 
+  Product.create!(name: Faker::Tea.variety, cost: Faker::Number.decimal(l_digits: 2), country_of_origin: Faker::Address.country_name_to_code)
+end
   
-5.times.do |index|
-  products = Product.all
-  products.each do |product|
-    Review.create!(author: Faker::Friends.character,
-      content_body: Faker::Hipster.paragraph_by_chars(characters: 200, supplemental:false))
-      rating: 
+250.times.do |index|
+  Review.create!(author: Faker::Name.name, content_body: Faker::Hipster.paragraph_by_chars(characters: 200, supplemental:false), rating: 1+rand(5), product_id: 1+rand(50))
+end 
 
 p "Created #{Product.count} products"
 p "Created #{Review.count} reviews"
-
-# in progress
