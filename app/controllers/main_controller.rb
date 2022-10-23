a5804669489ae5db855a2663381228931a1991c7
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-22 22:02:28 -0700

    Added main page and functionality for main page.

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,9 @@
+class MainController <ApplicationController
+  def index
+    @usa_made_products = Product.usa_made
+    @most_recent_products = Product.three_most_recent 
+    @most_reviews = Product.most_reviews 
+
+    render :index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  resources :products, except: [:destroy] do
-    resources :reviews, except: [:destroy]
+  root to: 'main#index'
+  resources :main, only: [:index]
+  resources :products do 
+    resources :reviews, except: [:destroy, :update, :edit]
   end 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-    root to: 'products#index'
-  end
 end
