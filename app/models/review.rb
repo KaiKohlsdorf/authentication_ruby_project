b86a6e5a5b5058b10899038a52d3c45fb57d235d
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-22 20:32:14 -0700

    12 tests passed. Added tests for numericality, length, and presence of reviews and products.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product 
+
+  validates :author, presence: true
+  validates :content_body, presence: true, length: { within: 50..250 }
+  validates :rating, presence: true, numericality: { integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
 end
